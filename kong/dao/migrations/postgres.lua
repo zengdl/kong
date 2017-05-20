346622af82c5c95bfb3f041ee0390eb9cc900465
Thibault Charbonnier <thibaultcha@me.com>
2017-05-19 17:32:59 -0700

    chore(migrations) remove 'nodes' table

--- kong/dao/migrations/postgres.lua
+++ kong/dao/migrations/postgres.lua
@@ -502,4 +502,10 @@ return {
       DROP TRIGGER IF EXISTS delete_expired_cluster_events_trigger;
     ]],
   },
+  {
+    name = "2017-05-19-173100_remove_nodes_table",
+    up = [[
+      DROP TABLE nodes;
+    ]],
+  },
 }
